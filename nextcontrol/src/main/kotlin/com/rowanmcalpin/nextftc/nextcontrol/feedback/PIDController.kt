bf014c9a5e9364d05811b41c96a42d6f2fe30c7d
Zach Harel <zach.harel42@gmail.com>
2025-03-07 19:35:52 -0500

    make PIDController better fit NextControl framework; fix tests

--- nextcontrol/src/main/kotlin/com/rowanmcalpin/nextftc/nextcontrol/feedback/PIDController.kt
+++ nextcontrol/src/main/kotlin/com/rowanmcalpin/nextftc/nextcontrol/feedback/PIDController.kt
@@ -3,80 +3,48 @@ package com.rowanmcalpin.nextftc.nextcontrol.feedback
 import com.rowanmcalpin.nextftc.nextcontrol.utils.KineticState
 
 /**
- * PID controller with various feedforward components.
- * Originally from Roadrunner 0.5
- * Ported to Kotlin by Zach.Waffle and j5155
+ * Traditional proportional-integral-derivative controller.
+ * @author rbrott
+ * @author j5155
+ * @author zach.waffle
  */
 open class PIDController @JvmOverloads constructor(
     var kP: Double, 
     var kI: Double = 0.0, 
     var kD: Double = 0.0
 ) {
-    /**
-     * Target position (that is, the controller setpoint).
-     */
-    var targetPosition: Int = 0
-
-    /**
-     * Target velocity.
-     */
-    var targetVelocity: Double = 0.0
 
     /**
      * Error computed in the last call to [calculate]
      */
     var lastError: Double = 0.0
+        private set
     private var errorSum = 0.0
     private var lastTimestamp = 0L
 
     open fun calculate(
         timestamp: Long,
-        error: KineticState
+        posError: Double,
+        velError: Double? = null
     ): Double {
-        val dt = (timestamp - lastTimestamp).toDouble()
-        errorSum += 0.5 * (error.position) * dt
-
-        lastError = error.position
-        lastTimestamp = timestamp
-
-        return kP * error.position + kI * errorSum + kD * error.velocity
-    }
-
-    open fun calculate(error: KineticState): Double = calculate(System.nanoTime(), error)
-
-    /**
-     * Run a single iteration of the controller.
-     *
-     * @param timestamp        measurement timestamp as given by [System.nanoTime]
-     * @param measuredPosition measured position (feedback)
-     * @param measuredVelocity measured velocity
-     */
-    @JvmOverloads
-    open fun calculate(
-        timestamp: Long,
-        measuredPosition: Double,
-        measuredVelocity: Double? = null
-    ): Double {
-        val error = targetPosition - measuredPosition
-
-        val dt = (timestamp - lastTimestamp).toDouble()
-        errorSum += 0.5 * (error + lastError) * dt
-        val errorDeriv = if (dt <= 0.0) 0.0 else (error - lastError) / dt
-
-        val velError = if (measuredVelocity == null) {
-            errorDeriv
-        } else {
-            targetVelocity - measuredVelocity
+        if (lastTimestamp == 0L) {
+            lastError = posError
+            lastTimestamp = timestamp
+            return kP * posError + kD * (velError ?: 0.0)
         }
 
-        return calculate(timestamp, KineticState(error, velError))
+        val dt = (timestamp - lastTimestamp).toDouble()
+        errorSum += (posError) * dt
+
+        lastError = posError
+        lastTimestamp = timestamp
+
+        val derivError = velError ?: ((posError - lastError) / dt)
+
+        return kP * posError + kI * errorSum + kD * derivError
     }
 
-    fun calculate(
-        measuredPosition: Double
-    ): Double {
-        return calculate(System.nanoTime(), measuredPosition)
-    }
+    fun calculate(posError: Double, velError: Double? = null) = calculate(System.nanoTime(), posError, velError)
 
     /**
      * Reset the controller's integral sum.
@@ -88,14 +56,34 @@ open class PIDController @JvmOverloads constructor(
     }
 }
 
-class PIDElement(
+/**
+ * PID controller that operates on positions.
+ * @param kP proportional gain
+ * @param kI integral gain
+ * @param kD derivative gain
+ */
+class PositionalPIDController @JvmOverloads constructor(
     kP: Double,
     kI: Double = 0.0,
-    kD: Double = 0.0
-) : FeedbackElement {
-    private val controller = PIDController(kP, kI, kD)
+    kD: Double = 0.0,
+) :  PIDController(kP, kI, kD), FeedbackElement {
+    fun calculate(timestamp: Long, error: KineticState) = this.calculate(timestamp, error.position, error.velocity)
 
-    override fun calculate(error: KineticState): Double {
-        return controller.calculate(error)
-    }
+    override fun calculate(error: KineticState) = this.calculate(error.position, error.velocity)
+}
+
+/**
+ * PID controller that operates on velocities.
+ * @param kP proportional gain
+ * @param kI integral gain
+ * @param kD derivative gain
+ */
+class VelocityPIDController @JvmOverloads constructor(
+    kP: Double,
+    kI: Double = 0.0,
+    kD: Double = 0.0,
+) : PIDController(kP, kI, kD), FeedbackElement {
+    fun calculate(timestamp: Long, error: KineticState) = this.calculate(timestamp, error.velocity, error.acceleration)
+
+    override fun calculate(error: KineticState) = this.calculate(error.velocity, error.acceleration)
 }
--- nextcontrol/src/test/java/com/rowanmcalpin/nextftc/nextcontrol/feedback/PIDFeedbackTest.kt
+++ nextcontrol/src/test/java/com/rowanmcalpin/nextftc/nextcontrol/feedback/PIDFeedbackTest.kt
@@ -8,37 +8,61 @@ class PIDFeedbackTest {
     @Test
     fun `returns 0 when all gains are 0`(){
         // Arrange
-        val pidFeedback = PIDElement(0.0, 0.0, 0.0)
+        val posPID = PositionalPIDController(0.0, 0.0, 0.0)
+        val velPID = VelocityPIDController(0.0, 0.0, 0.0)
 
         // Act
-        val feedback = pidFeedback.calculate(KineticState(20.0, 10.0))
+        val posFeedback = posPID.calculate(KineticState(20.0, 10.0))
+        val velFeedback = velPID.calculate(KineticState(20.0, 10.0))
 
         // Assert
-        assertEquals(0.0, feedback, 0.0)
+        assertEquals(0.0, posFeedback, 0.0)
+        assertEquals(0.0, velFeedback, 0.0)
     }
 
     @Test
     fun `returns 0 when error is 0`(){
         // Arrange
-        val pidFeedback = PIDElement(1.0, 1.0, 1.0)
+        val posPID = PositionalPIDController(1.0, 1.0, 1.0)
+        val velPID = VelocityPIDController(1.0, 1.0, 1.0)
 
         // Act
-        val feedback = pidFeedback.calculate(KineticState(0.0, 0.0))
+        val posFeedback = List<Double>(5) { posPID.calculate(KineticState(0.0, 0.0, 0.0)) }
+        val velFeedback = List<Double>(5) { velPID.calculate(KineticState(0.0, 0.0, 0.0)) }
 
         // Assert
-        assertEquals(0.0, feedback, 0.0)
+        posFeedback.forEach { assertEquals(0.0, it, 0.0) }
+        velFeedback.forEach { assertEquals(0.0, it, 0.0) }
     }
 
     @Test
     fun `returns error when kP is 1 and other gains are 0`(){
         // Arrange
-        val pidFeedback = PIDElement(1.0, 0.0, 0.0)
+        val posPID = PositionalPIDController(1.0, 0.0, 0.0)
+        val velPID = VelocityPIDController(1.0, 0.0, 0.0)
         val error = 10.0
 
         // Act
-        val feedback = pidFeedback.calculate(KineticState(error, 0.0))
+        val posFeedback = posPID.calculate(KineticState(10.0, 14.0))
+        val velFeedback = velPID.calculate(KineticState(27.0, 10.0, 97.0))
 
         // Assert
-        assertEquals(error, feedback, 0.0)
+        assertEquals(error, posFeedback, 0.0)
+        assertEquals(error, velFeedback, 0.0)
+    }
+
+    @Test
+    fun `integral sums correctly when kI is 1 and other gains are 0`(){
+        //Arrange
+        val posPID = PositionalPIDController(0.0, 1.0, 0.0)
+        val velPID = VelocityPIDController(0.0, 1.0, 0.0)
+
+        // Act
+        val posFeedback = List<Double>(5) { index -> posPID.calculate(1 + index.toLong(), KineticState(10.0, 0.0, 0.0)) }
+        val velFeedback = List<Double>(5) { index -> velPID.calculate(1 + index.toLong(), KineticState(0.0, 10.0, 0.0)) }
+
+        // Assert
+        posFeedback.forEachIndexed { index, it -> assertEquals(10.0 * index, it, 0.0) }
+        velFeedback.forEachIndexed { index, it -> assertEquals(10.0 * index, it, 0.0) }
     }
 }

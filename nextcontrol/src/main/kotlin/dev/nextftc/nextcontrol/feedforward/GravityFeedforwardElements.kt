34058966ea52e6a1935e5e1ce3b36d9c5d598691
rowan-mcalpin <87948250+rowan-mcalpin@users.noreply.github.com>
2025-03-12 15:34:24 -0700

    Make GravityFeedforwardParameters a data class and put kG as first parameter

--- nextcontrol/src/main/kotlin/dev/nextftc/nextcontrol/feedforward/GravityFeedforwardElements.kt
+++ nextcontrol/src/main/kotlin/dev/nextftc/nextcontrol/feedforward/GravityFeedforwardElements.kt
@@ -25,16 +25,16 @@ import kotlin.math.sign
 /**
  * Parameters for [ElevatorFeedforward] and [ArmFeedforward]
  *
+ * @param kG gravity value, added to overcome gravity
  * @param kV velocity gain, multiplied by the target velocity
  * @param kA acceleration gain, multiplied by the target acceleration
  * @param kS static gain, used to overcome static friction (multiplied by the sign of velocity)
- * @param kG gravity value, added to overcome gravity
  */
-class GravityFeedforwardParameters @JvmOverloads constructor(
+data class GravityFeedforwardParameters @JvmOverloads constructor(
+    @JvmField var kG: Double = 0.0,
     @JvmField var kV: Double = 0.0,
     @JvmField var kA: Double = 0.0,
-    @JvmField var kS: Double = 0.0,
-    @JvmField var kG: Double = 0.0
+    @JvmField var kS: Double = 0.0
 )
 
 /**
